ff033e53d3c2c02a7253a6cdaa0e29ca6ca93e78
jasonlpapadopoulos <87910990+jasonlpapadopoulos@users.noreply.github.com>
2024-12-13 10:37:07 -0500

    auth init

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlz2DgGutIzZxM0XgsBcXAczF37dSrezc",
+    authDomain: "plana-auth.firebaseapp.com",
+    projectId: "plana-auth",
+    storageBucket: "plana-auth.firebasestorage.app",
+    messagingSenderId: "722766994352",
+    appId: "1:722766994352:web:62b3c377022a3c153d1475",
+    measurementId: "G-7PJ0RDHV54"
+  };
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// Export Firebase services for use in other scripts
+export { app, auth };
